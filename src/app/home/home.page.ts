06013b1ac7603d96bcdeba6c99963f251cc7a2b3
Jesus Jimenez Cordero <jesus_heaven117@outlook.com>
2019-05-04 17:50:33 -0500

    Commit SoundAnimals

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ANIMALES } from '../data/data.animales';
+import { Animal } from '../interfaces/animales.interface';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,64 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+  animales: Animal[] = [];
+  audio = new Audio();
+  aundioTiempo: any;
+  ordenando: Boolean = true;
+
+  constructor() {
+    this.animales = ANIMALES.slice(0);
+  }
+
+  reproducir(animal: Animal) {
+    this.pausarAudio(animal);
+
+    if (animal.reproduciendo) {
+      animal.reproduciendo = false;
+      return;
+    }
+
+    console.log(animal);
+
+    this.audio.src = animal.audio;
+
+    this.audio.load();
+    this.audio.play();
+
+    animal.reproduciendo = true;
+
+    this.aundioTiempo = setTimeout(() => animal.reproduciendo = false, animal.duracion * 1000);
+  }
+
+  private pausarAudio(animalSeleccionado: Animal) {
+    clearTimeout(this.aundioTiempo);
+    this.audio.pause();
+    this.audio.currentTime = 0;
+
+    for (const animal of this.animales) {
+      if (animal.nombre !== animalSeleccionado.nombre) {
+        animal.reproduciendo = false;
+      }
+    }
+  }
+
+  borrarAnimal(idx: number) {
+    this.animales.splice(idx, 1);
+  }
+
+  recargarAnimales(refresh: any) {
+    setTimeout(() => {
+      console.log('Termino el refresh');
+      this.animales = ANIMALES;
+      refresh.target.complete();
+    }, 2000);
+  }
+
+  reorder(event) {
+    const item = this.animales.splice(event.detail.from, 1)[0];
+    this.animales.splice(event.detail.to, 0, item);
+    event.detail.complete();
+  }
 
 }
